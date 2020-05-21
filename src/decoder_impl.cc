4aaf4ba65ef231900410d7ebd0fbf2e36f6701b5
James Zern <jzern@google.com>
2020-05-20 20:20:15 -0700

    Automated rollback of commit 479edc656
    PiperOrigin-RevId: 312606601
    Change-Id: I67568db1a4d84eddcd0144b01899b571acf71cc3

--- src/decoder_impl.cc
+++ src/decoder_impl.cc
@@ -57,14 +57,10 @@ constexpr int kMaxLayers = 32;
 int GetBottomBorderPixels(const bool do_cdef, const bool do_restoration,
                           const bool do_superres) {
   int border = kBorderPixels;
-  if (do_cdef) {
-    border += kCdefBorder;
-  } else if (do_restoration) {
-    // If CDEF is enabled, loop restoration is safe without extra border.
-    border += kRestorationVerticalBorder;
-  }
-  if (do_superres) border += kSuperResVerticalBorder;
-  return Align(border, 2);  // Must be a multiple of 2.
+  if (do_cdef) border += 2 * kCdefBorder;
+  if (do_restoration) border += 2 * kRestorationVerticalBorder;
+  if (do_superres) border += 2 * kSuperResVerticalBorder;
+  return border;
 }
 
 // Sets |frame_scratch_buffer->tile_decoding_failed| to true (while holding on
